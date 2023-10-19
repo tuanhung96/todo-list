80ad35f0b8481d61f523a16725548e3bddcbd902
tuanhung96 <tuanhung.hn96@gmail.com>
2023-10-19 11:04:47 +0700

    update localstorage

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ button.addEventListener("click", () => {
     li.appendChild(span);
   }
   inputBox.value = "";
+  saveDate();
 });
 
 listContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
+    saveDate();
   } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
+    saveDate();
   }
 });
 
@@ -31,3 +34,5 @@ function saveDate() {
 function getData() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
+
+getData();
