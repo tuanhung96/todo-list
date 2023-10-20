ce5d569ee9a872fbd9f7fcabb3cb12142dd4b609
tuanhung96 <tuanhung.hn96@gmail.com>
2023-10-20 09:34:35 +0700

    20/10

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const inputBox = document.getElementById("input-box");
+const input = document.querySelector("input");
 const listContainer = document.getElementById("list-container");
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-  if (inputBox.value === "") {
+  if (input.value === "") {
     alert("You must enter you task!");
   } else {
     let li = document.createElement("li");
-    li.innerText = inputBox.value;
+    li.innerText = input.value;
     listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
-  inputBox.value = "";
+  input.value = "";
   saveData();
 });
 
 listContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
-    saveDate();
+    saveData();
   } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
-    saveDate();
+    saveData();
   }
 });
 
